cf74748306e51a0f8e17489d72b1eeb8f6997e42
Tushar Khairnar <tushark@itarium.local>
2019-01-01 19:46:02 +0530

    Linked list basic

---
+++ src/LinkedListDemo1.java
@@ -0,0 +1,18 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinkedListDemo1 {
+
+	public static void main(String[] args) {
+		List list = new LinkedList();
+			list.add("Object Oriented Programming");
+			list.add("Angular");
+			list.add("java");
+			list.add("Document DB");
+			list.add("MongoDB");
+			list.add("git");
+		System.out.println(list.toString());
+	}
+
+
+}
