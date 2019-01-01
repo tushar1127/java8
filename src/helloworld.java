a51bb9019c28b85f46d945071780aacf64009db4
Tushar Khairnar <tushark@itarium.local>
2019-01-01 19:26:22 +0530

    first java program commited to git

---
+++ src/helloworld.java
@@ -0,0 +1,12 @@
+class Message{
+	Message(String message){
+		System.out.println(message);
+	}
+}
+
+
+public class helloworld {
+	public static void main(String args[]) {
+		Message m = new Message("Hello to java again!");
+	}
+}
